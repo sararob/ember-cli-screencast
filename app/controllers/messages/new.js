7ccc5c9c438cee2a6bfad061600b25787c2ef3cc
Bill Heaton <bill@mediaartslab.com>
2015-04-21 20:33:09 -0700

    Use a default name for new chat messages

--- app/controllers/messages/new.js
+++ app/controllers/messages/new.js
@@ -1,17 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    name: 'Guest' + Math.floor(Math.random() * 100),
+    body: '',
+
     actions: {
         addMessage: function() {
             var newMessage = this.store.createRecord('message', {
                 name: this.get('name'),
                 body: this.get('body')
             });
-            newMessage.save();
-            this.setProperties({
-                name: '',
-                body: ''
-            });
+            newMessage.save().then(function() {
+                this.set('body', '');
+            }.bind(this));
         }
     }
 });
